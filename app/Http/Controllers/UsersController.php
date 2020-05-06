48ee67259de008a79887f2b232e889dcb0b9eff7
MJ <mj@example.com>
2020-05-06 09:40:31 +0000

    用户微博列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -32,7 +32,7 @@ class UsersController extends Controller
 
     public function show(User $user)
     {
-        // $statuses = $user->statuses()->orderBy('create_at', 'desc')->paginate(10);
+        $statuses = $user->statuses()->orderBy('created_at', 'desc')->paginate(10);
         return view('users.show', compact('user', 'statuses'));
     }
 
